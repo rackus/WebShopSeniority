2be1fe00e73d084df410e1f537e849986f473bb0
Wojtek <rak.wojciech@gmail.com>
2018-09-24 23:48:29 +0200

    App scaffolding and auth part 1
    project's scaffolding
    integration with firebase
    authentication - part1

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyAkUJEoy1UTGOG4_PpSdmHX1CvJJNKRSYY",
+    authDomain: "webshop-seniority.firebaseapp.com",
+    databaseURL: "https://webshop-seniority.firebaseio.com",
+    projectId: "webshop-seniority",
+    storageBucket: "webshop-seniority.appspot.com",
+    messagingSenderId: "81815131040"
+  }
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,15 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase: {
+    apiKey: "AIzaSyAkUJEoy1UTGOG4_PpSdmHX1CvJJNKRSYY",
+    authDomain: "webshop-seniority.firebaseapp.com",
+    databaseURL: "https://webshop-seniority.firebaseio.com",
+    projectId: "webshop-seniority",
+    storageBucket: "webshop-seniority.appspot.com",
+    messagingSenderId: "81815131040"
+  }
 };
 
 /*
